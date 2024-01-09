740b2182685295ba594163f0fa8ead61df8834bf
Taylor <taylor@bminvestments.co.za>
2024-01-09 16:55:18 +0200

    Made fe changes on menu and color on client index, also trying to fix bootstrap conflict on mobile friendly menu

--- src/App.vue
+++ src/App.vue
@@ -5,18 +5,29 @@ import { mapState, mapActions } from 'vuex';
 <template>
   <div>
     <header>
+      <meta name="viewport" content="width=device-width, initial-scale=1">
       <div class="wrapper">
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
           <div class="container-fluid">
             <RouterLink class="navbar-brand" to="#">LOGO</RouterLink>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
+            <button 
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+            >
+            <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
               <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                 <li class="nav-item">
                   <RouterLink class="nav-link active" aria-current="page" to="/dashboard">Home</RouterLink>
+                </li>
+                <li class="nav-item">
+                  <RouterLink class="nav-link active" aria-current="page" to="">About us</RouterLink>
                 </li>
                 <li class="nav-item">
                   <RouterLink class="nav-link" to="/clients">Client</RouterLink>
@@ -57,7 +68,39 @@ export default {
 </script>
 
 <style>
+  .wrapper {
+    padding: 15px;
+  }
 
+  .navbar-brand {
+    font-size: 1.5rem;
+  }
+
+  .navbar-toggler {
+    border: none; 
+  }
+
+  .navbar-nav {
+    margin-top: 10px;
+  }
+
+  .nav-link {
+    padding: 10px;
+  }
+
+  @media (max-width: 767px) {
+    .navbar-toggler {
+      margin-top: 5px;
+    }
+
+    .navbar-nav {
+      margin-top: 0;
+    }
+
+    .nav-link {
+      padding: 10px 15px;
+    }
+  }
 </style>
 
 
